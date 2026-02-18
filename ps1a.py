40057cce72114260b2e9f87fa798e9ad4da6cb7d
deepikaprakash0p-tech <deepikaprakash0p@gmail.com>
2026-02-18 22:27:05 +0530

    ps1a.py

--- ps1a.py
+++ ps1a.py
@@ -3,7 +3,7 @@ annual_salary=int(input("Enter your annual salary : "))
 portion_saved=float(input("Enter monthly saving rate : "))
 total_cost=int(input("Enter total cost of dream house : "))
 
-#Constants given in assignment
+#Constants taken as given in assignment
 postion_of_down_payment=0.25
 annual_return=0.04
 
@@ -13,7 +13,7 @@ monthly_salary=annual_salary/12
 down_payment=total_cost*postion_of_down_payment
 months=0
 
-#Loops
+#Loop to calculate required month
 while current_savings<down_payment:
       current_savings+=current_savings*(annual_return/12)
       current_savings+=monthly_salary*portion_saved
@@ -22,3 +22,4 @@ while current_savings<down_payment:
 #Output to show the no.of months
 
 print(f"Number of months : {months}")
+
